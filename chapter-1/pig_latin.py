9e5f3676e9e551570e36944f5861c548067a7bf2
Saidamir-Sh <saidamirpl@gmail.com>
2024-01-20 15:43:41 +0100

    pig latin

--- chapter-1/pig_latin.py
+++ chapter-1/pig_latin.py
@@ -5,22 +5,23 @@ def pig_latin():
     """Accept english word as input and translate it to Pig Latin."""
     print("Welcome to Pig Latin\n")
 
-    while True:
+    vowels = ["a","e","i","o","u"]
 
+    while True:
         word = input("Enter a word: ")
 
-        if not word.isalpha():
-            print("Please check your input, it should contain only letters !", file=sys.stderr)
+        if word.isalpha():
+            if word[0].lower() in vowels:
+                print(f"Translation: {word}way\n\n")
+            else:
+                print(f"Translation: {word[1:]}{word[0]}ay\n\n")
+        else:
+            print("Enter only letters", file=sys.stderr)
 
-        try_again = input("Play again ? (Press Enter to continue or press n to quit.) ")
-
-        if try_again.lower() not in ['', 'n']:
-            print("Entered invalid choice", file=sys.stderr)
+        try_again = input("Continue ? (Press Enter or n to quit.)")
 
         if try_again.lower() == "n":
             break
 
 if __name__ == "__pig_latin__":
     pig_latin()
-
-pig_latin()
